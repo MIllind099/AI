75da20bc1f9cdb5e40ce6d54e3adb8b3d8d05fce
MIllind099 <millind729@gmail.com>
2026-08-27 16:57:06 +0530

    Delete samadhan-setu-with-ai-duplicate-detection directory

--- samadhan-setu-with-ai-duplicate-detection/backend/routes/proposalRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { selectProposal } = require('../controllers/proposalController');
-const { protect, authorize } = require('../middleware/auth');
-
-router.patch('/:id/select', protect, authorize('admin'), selectProposal);
-
-module.exports = router;
